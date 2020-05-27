adeec7841cd99ff81852ea747493c73ad27f88e0
ksv1986 <ksv0x07c2@gmail.com>
2020-05-27 19:36:42 +0300

    Prepare component project

--- foobar2000/foo_vis_projectm_ng/dllmain.cpp
+++ foobar2000/foo_vis_projectm_ng/dllmain.cpp
@@ -1,19 +1,13 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
-}
+VALIDATE_COMPONENT_FILENAME("foo_vis_projectm_ng.dll");
 
+// Declaration of your component's version information
+// Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
+// Note that it is possible to declare multiple components within one DLL, but it's strongly recommended to keep only one declaration per DLL.
+// As for 1.1, the version numbers are used by the component update finder to find updates; for that to work, you must have ONLY ONE declaration per DLL. If there are multiple declarations, the component is assumed to be outdated and a version number of "0" is assumed, to overwrite the component with whatever is currently on the site assuming that it comes with proper version numbers.
+DECLARE_COMPONENT_VERSION("projectM Visualization",
+"0.1",
+"Visualization based on projectM\n"
+"Copyright (C) Andrey Kuleshov 2020\n"
+);
--- foobar2000/foo_vis_projectm_ng/pch.h
+++ foobar2000/foo_vis_projectm_ng/pch.h
@@ -7,7 +7,6 @@
 #ifndef PCH_H
 #define PCH_H
 
-// add headers that you want to pre-compile here
-#include "framework.h"
+#include <helpers/foobar2000+atl.h>
 
 #endif //PCH_H
